6ddd7bc660a4abb03d9c3beae144b4cc55078d6f
Geoff Rollins <grollins@gmail.com>
2014-02-27 09:54:32 -0800

    Initial south migrations

---
+++ flexe_web/jobs/migrations/0001_initial.py
@@ -0,0 +1,103 @@
+# -*- coding: utf-8 -*-
+from south.utils import datetime_utils as datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Adding model 'Job'
+        db.create_table(u'jobs_job', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('status', self.gf('model_utils.fields.StatusField')(default='submitted', max_length=100, no_check_for_status=True)),
+            ('status_changed', self.gf('model_utils.fields.MonitorField')(default=datetime.datetime.now, monitor=u'status')),
+            ('title', self.gf('django.db.models.fields.CharField')(default='', max_length=100, blank=True)),
+            ('reference', self.gf('django.db.models.fields.files.FileField')(max_length=100)),
+            ('comparison', self.gf('django.db.models.fields.files.FileField')(max_length=100)),
+            ('owner', self.gf('django.db.models.fields.related.ForeignKey')(related_name='jobs', to=orm['auth.User'])),
+            ('created_on', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+        ))
+        db.send_create_signal(u'jobs', ['Job'])
+
+        # Adding unique constraint on 'Job', fields ['owner', 'title']
+        db.create_unique(u'jobs_job', ['owner_id', 'title'])
+
+        # Adding model 'Result'
+        db.create_table(u'jobs_result', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('job', self.gf('django.db.models.fields.related.ForeignKey')(related_name='result', to=orm['jobs.Job'])),
+            ('rmsd', self.gf('django.db.models.fields.FloatField')(default=-99.9)),
+        ))
+        db.send_create_signal(u'jobs', ['Result'])
+
+
+    def backwards(self, orm):
+        # Removing unique constraint on 'Job', fields ['owner', 'title']
+        db.delete_unique(u'jobs_job', ['owner_id', 'title'])
+
+        # Deleting model 'Job'
+        db.delete_table(u'jobs_job')
+
+        # Deleting model 'Result'
+        db.delete_table(u'jobs_result')
+
+
+    models = {
+        u'auth.group': {
+            'Meta': {'object_name': 'Group'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '80'}),
+            'permissions': ('django.db.models.fields.related.ManyToManyField', [], {'to': u"orm['auth.Permission']", 'symmetrical': 'False', 'blank': 'True'})
+        },
+        u'auth.permission': {
+            'Meta': {'ordering': "(u'content_type__app_label', u'content_type__model', u'codename')", 'unique_together': "((u'content_type', u'codename'),)", 'object_name': 'Permission'},
+            'codename': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'content_type': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['contenttypes.ContentType']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50'})
+        },
+        u'auth.user': {
+            'Meta': {'object_name': 'User'},
+            'date_joined': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'email': ('django.db.models.fields.EmailField', [], {'max_length': '75', 'blank': 'True'}),
+            'first_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'groups': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Group']"}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'is_staff': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'is_superuser': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
+            'last_login': ('django.db.models.fields.DateTimeField', [], {'default': 'datetime.datetime.now'}),
+            'last_name': ('django.db.models.fields.CharField', [], {'max_length': '30', 'blank': 'True'}),
+            'password': ('django.db.models.fields.CharField', [], {'max_length': '128'}),
+            'user_permissions': ('django.db.models.fields.related.ManyToManyField', [], {'symmetrical': 'False', 'related_name': "u'user_set'", 'blank': 'True', 'to': u"orm['auth.Permission']"}),
+            'username': ('django.db.models.fields.CharField', [], {'unique': 'True', 'max_length': '30'})
+        },
+        u'contenttypes.contenttype': {
+            'Meta': {'ordering': "('name',)", 'unique_together': "(('app_label', 'model'),)", 'object_name': 'ContentType', 'db_table': "'django_content_type'"},
+            'app_label': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
+        },
+        u'jobs.job': {
+            'Meta': {'ordering': "('created_on',)", 'unique_together': "(('owner', 'title'),)", 'object_name': 'Job'},
+            'comparison': ('django.db.models.fields.files.FileField', [], {'max_length': '100'}),
+            'created_on': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'owner': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'jobs'", 'to': u"orm['auth.User']"}),
+            'reference': ('django.db.models.fields.files.FileField', [], {'max_length': '100'}),
+            'status': ('model_utils.fields.StatusField', [], {'default': "'submitted'", 'max_length': '100', u'no_check_for_status': 'True'}),
+            'status_changed': ('model_utils.fields.MonitorField', [], {'default': 'datetime.datetime.now', u'monitor': "u'status'"}),
+            'title': ('django.db.models.fields.CharField', [], {'default': "''", 'max_length': '100', 'blank': 'True'})
+        },
+        u'jobs.result': {
+            'Meta': {'object_name': 'Result'},
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'job': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'result'", 'to': u"orm['jobs.Job']"}),
+            'rmsd': ('django.db.models.fields.FloatField', [], {'default': '-99.9'})
+        }
+    }
+
+    complete_apps = ['jobs']
